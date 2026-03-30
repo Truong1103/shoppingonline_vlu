e1194be916086477c50aa4df39fc615983eab2b1
Truong Nguyen <107033368+Truong1103@users.noreply.github.com>
2026-03-30 12:59:54 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.k9yzif8.mongodb.net',
+    DB_USER: 'taikon4725',
+    DB_PASS: 'taikon4725',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'taikon4725shoppingonline',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'gymptw1@gmail.com', // gmail service
+    EMAIL_PASS: 'ylgl cqxp drvp qarz'
+  };
+  module.exports = MyConstants;
