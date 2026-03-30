e851a262c672095e6516f1ae1b8afa4545bf3b9c
Truong Nguyen <107033368+Truong1103@users.noreply.github.com>
2026-03-30 13:01:14 +0700

    admin

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="align-center">
+                <h2 className="text-center">ADMIN HOME</h2>
+                <img
+                    src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3Y3MHUwdzdwMXAzNDI0aGJzZzg0OW03YTUweW1mcHMxMXIwcmRvciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MEFVcuRIoVETUMYZEe/giphy.gif"
+                    width="800px"
+                    height="600px"
+                    alt=""
+                />
+            </div>
+        );
+    }
+}
+
+export default Home;
